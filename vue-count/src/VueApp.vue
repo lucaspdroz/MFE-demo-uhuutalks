16041d808cd2a047582751a4fd594867eb2fbdf3
Lucas Pacheco <lucaspdroz@gmail.com>
2022-12-05 00:24:06 -0300

    feat: adding vue-count mfe project with vue-countdown timer

---
+++ vue-count/src/VueApp.vue
@@ -0,0 +1,19 @@
+<script setup>
+import VueCountdown from '@chenfengyuan/vue-countdown'
+import Header from './components/Header'
+</script>
+
+<template>
+  <Header />
+  <vue-countdown
+    :time="2 * 24 * 60 * 60 * 1000"
+    v-slot="{ days, hours, minutes, seconds }"
+  >
+    Time Remaining：{{ days }} days, {{ hours }} hours, {{ minutes }} minutes,
+    {{ seconds }} seconds.
+  </vue-countdown>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
---
+++ vue-count/src/bootloader.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import "./index.css";
+import VueApp from "./VueApp.vue";
+
+createApp(VueApp).mount("#vue-app");
+
